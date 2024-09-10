5c34ca4885da7ad4c9df96d637413f5a3a9949d6
Cliff Njoroge <37869706+cliffordkleinsr@users.noreply.github.com>
2024-09-10 14:13:04 +0300

    stable_CloudflareV1

--- src/app.d.ts
+++ src/app.d.ts
@@ -30,7 +30,15 @@ declare global {
 			clients: Client,
 			available_survs: SurveyData
 		}
-		// interface Platform {}
+		interface Platform {
+			env: {
+				COUNTER: DurableObjectNamespace;
+			};
+			context: {
+				waitUntil(promise: Promise<any>): void;
+			};
+			caches: CacheStorage & { default: Cache }
+		}
 	}
 }
 
